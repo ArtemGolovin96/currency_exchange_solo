a218b24a79309487910bc4ebf3925615ad193ec5
Artem Golovin <artempotapkin96@icloud.com>
2021-04-20 15:44:46 +0300

    step 1

---
+++ script.js
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>Курсы валют</title>
+    <meta name="description" content="Сольный проект - Курсы валют" />
+    
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <script type="text/javascript" src="./index.js"></script>
+  </head>
+  <body>
+    <header>
+      Шапка
+      <nav>
+        Навигация
+      </nav>
+    </header>
+    <main>
+      <section>
+        <h1>Заголовок страницы</h1>
+        Основная часть
+      </section>
+    </main>
+    <aside>
+      Боковая колонка (сайдбар)
+    </aside>
+    <footer>
+      Подвал
+    </footer>
+  </body>
+</html>
