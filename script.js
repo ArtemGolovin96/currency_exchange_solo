98ff7585d3f914cd722035b7d07bcd6413e2d015
Artem Golovin <artempotapkin96@icloud.com>
2021-04-30 13:57:43 +0300

    доработки

--- script.js
+++ script.js
@@ -1,27 +1,131 @@
 //Получеие кнопок
 // I Have section
+let from = 'RUB';
+let to = 'USD';
+let data1 = 0;
+let data = 0; 
+let timeOutId;
+function addEventButtonsL () {
+  let valuteButtons = document.querySelectorAll('.buttonv');
+  valuteButtons.forEach((el) => { 
+    el.addEventListener('click', (e) => { 
+      from = el.textContent;
+      getFetch();
+        let selectValuteButton = document.querySelector('.select');
+        selectValuteButton.classList.remove('optionactive');
+        valuteButtons.forEach((item) => {
+          if(item.classList.contains('buttonfocus')) {
+            item.classList.remove('buttonfocus');
+          }
+        })
+        e.target.classList.toggle('buttonfocus');
+        inputValue();
+    })
+  })  
+}
 
 
 
-let valuteButtons = document.querySelectorAll('.buttonv');
-valuteButtons.forEach((el) => { 
-  el.addEventListener('click', (e) => {
-    console.log(e.target.textContent);
-    el.style.backgroundColor = '#833AE0'
-    el.style.color = 'white'
+
+function addEventButtonsR () {
+  let valuteButtons2 = document.querySelectorAll('.buttonv2');
+  valuteButtons2.forEach((el) => { 
+  el.addEventListener('click', (e) => { 
+    to = el.textContent;
+    getFetch();
+    let selectValuteButton2 = document.querySelector('.select2');
+    selectValuteButton2.classList.remove('optionactive');
+      valuteButtons2.forEach((item) => {
+        if(item.classList.contains('buttonfocus2')) {
+          item.classList.remove('buttonfocus2');
+        }
+      })
+      e.target.classList.toggle('buttonfocus2');
+      inputValue();
   })
 })  
+}
 
-let selectValuteButton = document.querySelectorAll('.select');
-selectValuteButton.forEach((el) => {
+
+function addEventSelectL () {
+  let selectValuteButton = document.querySelectorAll('.select');
+  selectValuteButton.forEach((el) => {
   el.addEventListener('change', (e) => {
-    console.log(e.target.value);
+    from = e.target.options[e.target.selectedIndex].textContent;
+    getFetch();
+    el.classList.toggle('optionactive');
+    let valuteButtons = document.querySelectorAll('.buttonv');
+    valuteButtons.forEach((item) => {
+      item.classList.remove('buttonfocus');
+    })
+    inputValue();
   })
-})  
+}) 
+}
+
+function addEventSelectR () {
+  let selectValuteButton2 = document.querySelectorAll('.select2');
+  selectValuteButton2.forEach((el) => {
+    el.addEventListener('change', (e) => {
+      to = e.target.options[e.target.selectedIndex].textContent;
+      getFetch();
+      el.classList.toggle ('optionactive');
+      let valuteButtons2 = document.querySelectorAll('.buttonv2');
+      valuteButtons2.forEach((item) => {
+        item.classList.remove('buttonfocus2');
+      })
+      inputValue();
+    })
+  }) 
+}
+
+function inputValue() {
+    let inputL = document.querySelector('.inputihave');
+    let inputR = document.querySelector('.inputiwant');
+    inputL.value = '';
+    inputR.value = '';
+    inputL.addEventListener('input', (e) => {
+      if(from === to){ 
+        inputR.value = inputL.value
+      }  else {
+        clearTimeout(timeOutId);
+        timeOutId = setTimeout(() => {
+          inputR.value = (inputL.value * data.rates[to]).toFixed(4);
+        }, 2000)
+      }
+    })
+  }
+
+
+
+inputValue();
+addEventButtonsL();
+addEventSelectL();
+addEventButtonsR();
+addEventSelectR();
+
+
 
 async function getFetch(){
   let respose =  await fetch(`https://api.ratesapi.io/api/latest?base=${from}&symbols=${to}`);
-  let data = await respose.json();
-  console.log(data);
+  let respose1 =  await fetch(`https://api.ratesapi.io/api/latest?base=${to}&symbols=${from}`);
+  data1 = await respose1.json();
+  data = await respose.json();
+    Promise.all([data1, data])
+    .then(items => {
+      let pL = document.querySelector('#inputp1');
+      pL.textContent = `1 ${data.base} =  ${data.rates[to]} ${to}`;
+      let pR = document.querySelector('#inputp2');
+      pR.textContent = `1 ${to} =  ${data1.rates[from]} ${from}`;
+    })
+    .catch(err => {
+      console.log('errorrrrr')
+    })
+
 }
+
+
+
+
+
 
